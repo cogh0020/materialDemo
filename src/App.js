71c4d629be993df389a682010d8d3266f533ab77
--replace-all <cogh0020@algonquinlive.com>
2022-10-19 20:08:10 -0400

    New commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Container from '@mui/material/Container'
+import Header from './Header'
+import Card from './Card'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([])
+
+  useEffect(()=>{
+    fetch(`https://fakestoreapi.com/products/`)
+    .then(res=> res.json())
+    .then(data => setData(data))
+    .catch(err => console.log(err))
+  }, [])
+
+    return (
+      <div className="App">
+        <Container>
+          <Header/>
+          <Card data={data} />
+        </Container>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header(){
+
+  return(
+    <>
+    <h1>My Store</h1>
+    </>
+  )
+}
+
+export default Header
